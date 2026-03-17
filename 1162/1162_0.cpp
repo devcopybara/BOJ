ce997264e3bf18705d80d61c6204a76d456970bd
copybara <dev.copybara@google.com>
2026-03-17 23:41:08 +0900

    Expert20260317:1162

--- 1162/1162_0.cpp
+++ 1162/1162_0.cpp
@@ -7,7 +7,7 @@ const uint64_t MX_N = 10'005;
 const uint64_t MX_K = 21;
 const uint64_t INF = MX_N * 1'000'000;
 vector<pii> adj[MX_N];
-uint64_t dist[MX_N][MX_K];
+uint64_t dist[MX_K][MX_N];
 priority_queue<ti3,vector<ti3>,greater<ti3>> pq;
 
 int main() {
@@ -25,32 +25,32 @@ int main() {
     }
 
     for(uint64_t i = 0; i <= k; i++) 
-        fill(dist[i]+1,dist[i]+n,INF);
+        fill(dist[i]+1,dist[i]+1+n,INF);
 
-    dist[1][0] = 0;
+    dist[0][1] = 0;
     pq.push({0,0,1});
     while(!pq.empty()) {
         auto [curD,curK,curV] = pq.top(); pq.pop();
-        if(curD != dist[curV][curK]) continue;
+        if(curD != dist[curK][curV]) continue;
 
         for(auto [nxtW, nxtV] : adj[curV]) {
             uint64_t nxtK = curK + 1;
             
-            if(dist[nxtV][curK] > curD + nxtW) {
-                dist[nxtV][curK] = curD + nxtW;
-                pq.push({dist[nxtV][curK],curK,nxtV});
+            if(dist[curK][nxtV] > curD + nxtW) {
+                dist[curK][nxtV] = curD + nxtW;
+                pq.push({dist[curK][nxtV],curK,nxtV});
             }
             
             if(curK == k) continue;
-            if(dist[nxtV][nxtK] > curD + nxtW) {
-                dist[nxtV][nxtK] = curD + nxtW;
-                pq.push({dist[nxtV][nxtK],nxtK,nxtV});
+            if(dist[nxtK][nxtV] > curD) {
+                dist[nxtK][nxtV] = curD;
+                pq.push({dist[nxtK][nxtV],nxtK,nxtV});
             }
         }
     }
 
-    uint64_t ans = 0;
+    uint64_t ans = INF;
     for(uint64_t i = 0; i <= k; i++)
-        ans = min(ans,dist[n][i]);
+        ans = min(ans,dist[i][n]);
     cout << ans;
 }
